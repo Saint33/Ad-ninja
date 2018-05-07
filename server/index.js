9da471a8ffad4f23c8bbdf0039471405d2a36d79
Saint33 <bnsaintz@gmail.com>
2018-05-07 11:34:21 +0300

    server minor changes

--- server/index.js
+++ server/index.js
@@ -18,13 +18,13 @@ mongoose.connect(config.DATABASE);
 app.use(express.static('client/build'));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     next();
+//   });
 
 
 
